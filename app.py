a74318d101c5b26db6035d4e8b7f73fa93adab6b
Md Saif Alam <mdsaifalamofficial@gmail.com>
2026-01-24 19:47:03 +0530

    Create app.py
    Added Flask backend

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    marks = request.form["marks"]
+
+    return f"Hello {name}, your marks are {marks}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
